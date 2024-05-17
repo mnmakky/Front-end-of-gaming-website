5b3046eb718a4e0433e0fb02f9e56dc81a73966c
mnmakky <159957993+mnmakky@users.noreply.github.com>
2024-05-17 18:02:18 +0100

    New

---
+++ script.js
@@ -0,0 +1,67 @@
+
+window.addEventListener('scroll',function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 0);
+});
+
+ /* filterable cards */
+let list = document.querySelectorAll('.list');
+let card = document.querySelectorAll('.card');
+
+    for(let i = 0; i<list.length; i++){
+        list[i].addEventListener('click',function(){
+         for(let j=0; j<list.length; j++){
+            list[j].classList.remove('active');
+         }
+         this.classList.add('active');
+
+         let dataFilter = this.getAttribute('data-filter');
+
+         for(let k=0; k<card.length; k++)
+         {
+            card[k].classList.remove('active');
+            card[k].classList.add('hide');
+
+            if(card[k].getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+                card[k].classList.remove('hide');
+                card[k].classList.add('active');
+            }
+         }
+
+        })
+    }
+
+    /* popup */
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle('active');
+}
+
+function togglePopupt(){
+    document.getElementById("popup-2").classList.toggle('active');
+}
+
+function togglePopupth(){
+    document.getElementById("popup-3").classList.toggle('active');
+}
+
+
+/* Scrolling Animation Effects*/
+window.addEventListener('scroll',function(){
+    var anime = document.querySelectorAll('.animeX');
+
+    for(var s = 0; s < anime.length; s++){
+        var windowheight = window.innerHeight;
+        var animetop = anime[s].getBoundingClientRect().top;
+        var animepoint = 150;
+
+       if(animetop < windowheight - animepoint){
+        anime[s].classList.add('active');
+       }
+       else{
+        anime[s].classList.remove('active');
+       }
+    }
+})
+
+
+
